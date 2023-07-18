77e13c6435400c7f253dc10483b864919eaa9e34
seth <getchoo@tuta.io>
2023-07-18 13:45:02 -0400

    chore: add prettier

--- src/stages/install.ts
+++ src/stages/install.ts
@@ -10,11 +10,11 @@ export const install = async () => {
   startGroup("Install attic");
 
   const installScript = await fetch(
-    "https://raw.githubusercontent.com/zhaofengli/attic/main/.github/install-attic-ci.sh"
+    "https://raw.githubusercontent.com/zhaofengli/attic/main/.github/install-attic-ci.sh",
   ).then((r) => {
     if (!r.ok)
       throw new Error(
-        `Failed to fetch install script: ${r.status} ${r.statusText}`
+        `Failed to fetch install script: ${r.status} ${r.statusText}`,
       );
 
     return r.text();
