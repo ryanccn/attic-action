5f586db5bcfd1b09dc721dd5d52198f13a7a12a9
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-07-18 22:21:33 +0800

    merge steps

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { install } from "./stages/install";
 import { configure } from "./stages/configure";
+import { push } from "./stages/push";
 
 (async () => {
   await install();
   await configure();
+  await push();
 })().catch((e) => {
   console.error(e);
   process.exit(1);
--- src/post.ts
+++
@@ -1,8 +0,0 @@
-import { push } from "./stages/push";
-
-(async () => {
-  await push();
-})().catch((e) => {
-  console.error(e);
-  process.exit(1);
-});
