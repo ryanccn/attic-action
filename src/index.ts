b8f633b17f945553e7255c46469d90239b803ef7
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-07-19 09:26:36 +0800

    refactors

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
 import { install } from "./stages/install";
 import { configure } from "./stages/configure";
-import { push } from "./stages/push";
-import { getState, saveState } from "@actions/core";
 
-const isPost = !!getState("isPost");
-
-async function setup() {
+const main = async () => {
   await install();
   await configure();
-}
+};
 
-if (!isPost) {
-  saveState("isPost", "true");
-  setup();
-} else {
-  push();
-}
+main();
