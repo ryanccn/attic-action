00f4a8b31e8a88206a170afba2e822beaba19b04
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-07-20 09:08:36 +0800

    improve action.yml

--- flake.nix
+++ flake.nix
@@ -5,32 +5,27 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs =
-    { nixpkgs
-    , ...
-    }:
-    let
-      mkSystems = sys: builtins.map (arch: "${arch}-${sys}") [ "x86_64" "aarch64" ];
-      systems =
-        mkSystems "linux"
-        ++ mkSystems "darwin";
+  outputs = {nixpkgs, ...}: let
+    mkSystems = sys: builtins.map (arch: "${arch}-${sys}") ["x86_64" "aarch64"];
+    systems =
+      mkSystems "linux"
+      ++ mkSystems "darwin";
 
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
 
-      forEachSystem = fn:
-        forAllSystems (s: fn nixpkgsFor.${s});
-    in
-    {
-      devShells = forEachSystem (pkgs: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            actionlint
-            nodePackages.pnpm
-          ];
-        };
-      });
+    forEachSystem = fn:
+      forAllSystems (s: fn nixpkgsFor.${s});
+  in {
+    devShells = forEachSystem (pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          actionlint
+          nodePackages.pnpm
+        ];
+      };
+    });
 
-      formatter = forEachSystem (p: p.nixpkgs-fmt);
-    };
+    formatter = forEachSystem (p: p.nixpkgs-fmt);
+  };
 }
