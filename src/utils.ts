bb111555fe50c088deea35cdceaf89a544347e33
MisterOutofTime <lostchaos3@googlemail.com>
2023-08-04 18:31:23 +0200

    fix

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import { exec } from "@actions/exec";
 import {readFile} from "node:fs";
 
 export const saveStorePaths = async () => {
-	await exec("sh"["-c" "nix  path-info --all --json > /tmp/store-paths"]);
+	await exec("sh",["-c", "nix  path-info --all --json > /tmp/store-paths"]);
 };
 export const getStorePaths = async () => {
 	
