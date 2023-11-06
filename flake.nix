e566203de649e9448e8de8b09581880daeb3b487
seth <getchoo@tuta.io>
2023-11-05 19:54:49 -0500

    chore: don't instantiate nixpkgs in flake

--- flake.nix
+++ flake.nix
@@ -6,18 +6,16 @@
   };
 
   outputs = {nixpkgs, ...}: let
-    mkSystems = sys: builtins.map (arch: "${arch}-${sys}") ["x86_64" "aarch64"];
-    systems =
-      mkSystems "linux"
-      ++ mkSystems "darwin";
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
 
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
-
-    forEachSystem = fn:
-      forAllSystems (s: fn nixpkgsFor.${s});
+    forAllSystems = fn: nixpkgs.lib.genAttrs systems (system: fn nixpkgs.legacyPackages.${system});
   in {
-    devShells = forEachSystem (pkgs: {
+    devShells = forAllSystems (pkgs: {
       default = pkgs.mkShell {
         packages = with pkgs; [
           actionlint
@@ -26,9 +24,9 @@
       };
     });
 
-    formatter = forEachSystem (p: p.alejandra);
+    formatter = forAllSystems (p: p.alejandra);
 
-    packages = forEachSystem (p: let
+    packages = forAllSystems (p: let
       time = toString builtins.currentTime;
       test = p.runCommand "test-${time}" {} ''
         echo ${time} > $out
