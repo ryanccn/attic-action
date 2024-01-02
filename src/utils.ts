defe28e44a3503404ec442aff4031a9326500928
ErrorNoInternet <errornointernet@envs.net>
2024-01-02 15:48:42 +0800

    fix: TypeError when pushing

--- src/utils.ts
+++ src/utils.ts
@@ -6,5 +6,5 @@ export const saveStorePaths = async () => {
 	await exec("sh", ["-c", "nix path-info --all --json > /tmp/store-paths"]);
 };
 export const getStorePaths = async () => {
-	return (JSON.parse(await readFile("/tmp/store-paths", "utf8")) as { path: string }[]).map((path) => path.path);
+	return Object.keys(JSON.parse(await readFile("/tmp/store-paths", "utf8")) as { path: string }[]);
 };
