cb098d2d85adc5918f6de420a01060fbc9c6bace
ErrorNoInternet <errornointernet@envs.net>
2024-01-03 03:40:45 +0800

    fix: compatibility with Nix 2.18

--- src/utils.ts
+++ src/utils.ts
@@ -6,5 +6,12 @@ export const saveStorePaths = async () => {
 	await exec("sh", ["-c", "nix path-info --all --json > /tmp/store-paths"]);
 };
 export const getStorePaths = async () => {
-	return Object.keys(JSON.parse(await readFile("/tmp/store-paths", "utf8")) as { path: string }[]);
+	const rawStorePaths = JSON.parse(await readFile("/tmp/store-paths", "utf8")) as { path: string }[];
+
+	// compatibility with Nix 2.18
+	if (Array.isArray(rawStorePaths)) {
+		return rawStorePaths.map((path) => path.path); 
+	};
+
+	return Object.keys(rawStorePaths);
 };
