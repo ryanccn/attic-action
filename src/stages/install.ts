55e6e8d8534de45f16d81a5e04ed107476667827
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2024-10-06 22:05:47 +0800

    feat: use `nix profile` to install Attic

--- src/stages/install.ts
+++ src/stages/install.ts
@@ -1,32 +1,13 @@
 import * as core from "@actions/core";
 import { exec } from "@actions/exec";
 
-import { writeFile } from "node:fs/promises";
-import { tmpdir } from "node:os";
-import { join } from "node:path";
-
 export const install = async () => {
 	core.startGroup("Install attic");
 
 	core.info("Installing attic");
-	const installScript = await fetch(
-		"https://raw.githubusercontent.com/zhaofengli/attic/main/.github/install-attic-ci.sh",
-	).then((r) => {
-		if (!r.ok) {
-			core.setFailed(`Action failed with error: ${r.statusText}`);
-			core.endGroup();
-			process.exit(1);
-		}
-
-		return r.text();
-	});
 
 	try {
-		const installScriptPath = join(tmpdir(), "install-attic-ci.sh");
-
-		await writeFile(installScriptPath, installScript);
-		core.info("Running install script");
-		await exec("bash", [installScriptPath]);
+		await exec("nix", ["profile", "install", "github:NixOS/nixpkgs/nixpkgs-unstable#attic-client"]);
 	} catch (e) {
 		core.setFailed(`Action failed with error: ${e}`);
 	}
@@ -35,6 +16,6 @@ export const install = async () => {
 };
 
 export const isInstalled = async () => {
-	let return_code = await exec("attic", ["-V"]);
-	return return_code === 0;
+	let returnCode = await exec("attic", ["-V"]);
+	return returnCode === 0;
 };
--- src/utils.ts
+++ src/utils.ts
@@ -5,13 +5,14 @@ import { readFile } from "node:fs/promises";
 export const saveStorePaths = async () => {
 	await exec("sh", ["-c", "nix path-info --all --json > /tmp/store-paths"]);
 };
+
 export const getStorePaths = async () => {
 	const rawStorePaths = JSON.parse(await readFile("/tmp/store-paths", "utf8")) as { path: string }[];
 
 	// compatibility with Nix 2.18
 	if (Array.isArray(rawStorePaths)) {
-		return rawStorePaths.map((path) => path.path); 
-	};
+		return rawStorePaths.map((path) => path.path);
+	}
 
 	return Object.keys(rawStorePaths);
 };
