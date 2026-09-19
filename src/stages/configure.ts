16ccd45c39cf27b6ec110ceaa6e769e470502c08
Mark King <mark@vemek.co>
2026-09-19 06:40:01 +0100

    fix: don't save store paths when skip-push is true (#64)

--- src/stages/configure.ts
+++ src/stages/configure.ts
@@ -10,6 +10,7 @@ export const configure = async () => {
 		const cache = core.getInput("cache");
 		const token = core.getInput("token");
 		const skipUse = core.getInput("skip-use");
+		const skipPush = core.getInput("skip-push");
 
 		core.info("Logging in to Attic cache");
 		if (!INTERNAL_DRY_RUN) await exec("attic", ["login", "--set-default", cache, endpoint, token]);
@@ -21,8 +22,12 @@ export const configure = async () => {
 			if (!INTERNAL_DRY_RUN) await exec("attic", ["use", cache]);
 		}
 
-		core.info("Collecting store paths before build");
-		await saveStorePaths();
+		if (skipPush === "true") {
+			core.info("Not collecting store paths before build as skip-push is set to true");
+		} else {
+			core.info("Collecting store paths before build");
+			await saveStorePaths();
+		}
 	} catch (e) {
 		core.setFailed(`Action failed with error: ${e}`);
 	}
